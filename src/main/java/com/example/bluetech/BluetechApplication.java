027c2b475170adc7aec54f86f3141639cfb1dfbc
qhung <tranquochung0404@gmail.com>
2025-04-16 13:27:00 +0700

    update

--- src/main/java/com/example/bluetech/BluetechApplication.java
+++ src/main/java/com/example/bluetech/BluetechApplication.java
@@ -9,9 +9,15 @@ public class BluetechApplication {
 
 	public static void main(String[] args) {
 //		Dotenv dotenv = Dotenv.load();
-		Dotenv dotenv = Dotenv.configure().directory("/app").load();
-		System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
-		System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
+//		Dotenv dotenv = Dotenv.configure().directory("/app").load();
+//		System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
+//		System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
+
+
+
+		System.setProperty("AWS_ACCESS_KEY_ID", "AWS_ACCESS_KEY_ID");
+		System.setProperty("AWS_SECRET_ACCESS_KEY", "AWS_SECRET_ACCESS_KEY");
+
 
 		SpringApplication.run(BluetechApplication.class, args);
 	}
